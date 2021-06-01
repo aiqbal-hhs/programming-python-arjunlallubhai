8254e0e8b23f6663626c162660d31980b59eceb1
arjunlallubhai <80367001+arjunlallubhai@users.noreply.github.com>
2021-06-01 12:07:31 +1200

    Add files via upload

---
+++ drawing.py
@@ -0,0 +1,41 @@
+import turtle
+timmy = turtle.Turtle()
+timmy.color("purple")
+timmy.pensize(10)
+
+timmy.penup()
+timmy.goto(15, 70)
+timmy.pendown()
+
+
+timmy.fillcolor("yellow")
+timmy.begin_fill()
+timmy.forward(200)
+timmy.left(90)
+timmy.forward(200)
+timmy.left(90)
+timmy.forward(200)
+timmy.left(90)
+timmy.forward(200)
+timmy.left(90)
+timmy.end_fill()
+
+
+
+timmy.penup()
+timmy.goto(20, 275)
+timmy.pendown()
+
+timmy.color("purple")
+timmy.pensize(10)
+
+timmy.fillcolor("red")
+timmy.begin_fill()
+timmy.left(60)
+timmy.forward(100)
+timmy.right(60)
+timmy.forward(100)
+timmy.right(40)
+timmy.right(27)
+timmy.forward(100)
+timmy.end_fill()
